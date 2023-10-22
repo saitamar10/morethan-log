f9631e2eaa176d0b1c58086aa0245462819edc49
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2023-10-22 17:31:58 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -51,7 +51,7 @@ const CONFIG = {
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
-      label: "💬 Utterances",
+      label: "💬 İfadeler",
     },
   },
   cusdis: {
