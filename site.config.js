8b5d117f2187bed006062f12bcbb5f3a75400386
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2023-10-22 17:39:08 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Teknoloji Forumu",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Teknoloji Haberleri",
     bio: "Her hafta yeni haber.",
     email: "ismetceberr@gmail.com",
